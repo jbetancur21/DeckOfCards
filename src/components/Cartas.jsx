edbd2d927d74b82abefc7b5f0d95f996b43f9e99
artpolo04 <artpolo04@gmail.com>
2023-06-08 21:55:27 -0500

    08/06/2023

--- src/components/Cartas.jsx
+++ src/components/Cartas.jsx
@@ -4,12 +4,32 @@ import { useContext } from "react";
 import IdPlayerContext from "../context/IdPlayerContext";
 
 const Cartas = ({ playerOne, playerTwo }) => {
-  
   const { IdPlayerOne, IdPlayerTwo } = useContext(IdPlayerContext);
-
   const [deckPlayerOne, setDeckPlayerOne] = useState([]);
   const [deckPlayerTwo, setDeckPlayerTwo] = useState([]);
+  const [ternaCartas, setTernaCartas] = useState([]);
+  const [aux, setAux] = useState(0);
+  const [winner, setWinner] = useState([{ terna: 0 }, { cuarta: 0 }]);
 
+  const getCard = async () => {
+    const url = `https://deckofcardsapi.com/api/deck/${IdPlayerOne}/draw/?count=1`;
+    const { data } = await axios.get(url);
+    for (let i = 0; i < deckPlayerOne.length; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (deckPlayerOne[i].value === deckPlayerOne[j].value) {
+          if (deckPlayerOne[i].suit !== deckPlayerOne[j].suit) {
+            setAux(aux + 1);
+            console.log(aux);
+          }
+        }
+      }
+      if (aux === 3) {
+        setWinner({ terna: winner.terna + 1 }, { cuarta: winner.cuarta });
+      } else if (aux === 4) {
+        setWinner({ terna: winner.terna }, { cuarta: winner.cuarta + 1 });
+      }
+    }
+  };
   useEffect(() => {
     const getDeckPlayerOne = async () => {
       const url = `https://deckofcardsapi.com/api/deck/${IdPlayerOne}/draw/?count=10`;
@@ -23,18 +43,21 @@ const Cartas = ({ playerOne, playerTwo }) => {
     };
     getDeckPlayerOne();
     getDeckPlayerTwo();
-  }, []);
+  }, [1]);
 
   return (
     <div>
       <h2>{playerOne}</h2>
-      {deckPlayerOne.map(mazoUno=>(
+      {deckPlayerOne.map((mazoUno) => (
         <img key={mazoUno.code} src={mazoUno.image} />
       ))}
       <h2>{playerTwo}</h2>
-      {deckPlayerTwo.map(mazoDos=>(
+      {deckPlayerTwo.map((mazoDos) => (
         <img key={mazoDos.code} src={mazoDos.image} />
       ))}
+      <button className="botonTraer" onClick={getCard}>
+        Pedir Carta
+      </button>
     </div>
   );
 };
