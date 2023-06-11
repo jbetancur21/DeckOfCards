12c5f015deeb3c9d66988368f8a1e0127b8e0344
jbetancur21 <juan_betancur82191@elpoli.edu.co>
2023-06-11 16:39:16 -0500

    11/06/2023 16:39

--- src/components/Cartas.jsx
+++ src/components/Cartas.jsx
@@ -10,31 +10,34 @@ const Cartas = ({ playerOne, playerTwo }) => {
   const [aux, setAux] = useState(0);
   const [winner, setWinner] = useState([]);
 
-
-
-
   const getCard = async () => {
     const url = `https://deckofcardsapi.com/api/deck/${IdPlayerOne}/draw/?count=1`;
     const { data } = await axios.get(url);
 
+    setWinner(0)
     let positions = [];
     for (let i = 0; i < deckPlayerOne.length; i++) {
-      
       for (let j = 0; j < deckPlayerOne.length; j++) {
-        if (deckPlayerOne[i].value === deckPlayerOne[j].value) {
-          if (deckPlayerOne[i].suit !== deckPlayerOne[j].suit) {
-            console.log(j)
-              if(positions.indexOf(j)===-1){
-                positions.push(j);
-              }
-              
+        if (
+          deckPlayerOne[i].value === deckPlayerOne[j].value &&
+          deckPlayerOne[i].suit !== deckPlayerOne[j].suit
+        ) {
+          if (positions.indexOf(j) === -1) {
+            positions.push(j);
           }
         }
-       
       }
     }
-    setWinner(positions)
+    setWinner(positions.sort());
+
+    let x = 0;
+    while(positions.indexOf(x) !== -1){
+      x = Math.floor((Math.random() * (9 - 0 + 1)) + 0);
+    }
     
+    deckPlayerOne.splice(x,1,data.cards[0])
+
+    console.log(x)
   };
   useEffect(() => {
     const getDeckPlayerOne = async () => {
@@ -61,7 +64,7 @@ const Cartas = ({ playerOne, playerTwo }) => {
       {deckPlayerTwo.map((mazoDos) => (
         <img key={mazoDos.code} src={mazoDos.image} />
       ))}
-      <button className="botonTraer" onClick={()=>getCard()}>
+      <button className="botonTraer" onClick={() => getCard()}>
         Pedir Carta
       </button>
     </div>
