a6addbb4feca81f4468122b3a7262e64b7a7b76a
jbetancur21 <juan_betancur82191@elpoli.edu.co>
2023-06-12 16:36:40 -0500

    12/06/2023 16:36

--- src/context/DetGanadorContext.jsx
+++ src/context/DetGanadorContext.jsx
@@ -14,21 +14,18 @@ const DetGanadorProvider = ({children}) =>{
     
 
     const getCard = async () => {
+
+      //JUGADOR UNO --------------------------------------------------------------------------
         const url = `https://deckofcardsapi.com/api/deck/${IdPlayerOne}/draw/?count=1`;
         const { data } = await axios.get(url);
     
-        //setWinner(0);
-        /* console.log(winner.length) */
-        
-        if (winner.length < 8){
+        setWinner(0);
+        console.log(data.cards)
         let positions = [];
         
         for (let i = 0; i < deckPlayerOne.length; i++) {
           for (let j = 0; j < deckPlayerOne.length; j++) {
-            if (
-              deckPlayerOne[i].value === deckPlayerOne[j].value &&
-              deckPlayerOne[i].suit !== deckPlayerOne[j].suit
-            ) {
+            if (deckPlayerOne[i].value === deckPlayerOne[j].value && deckPlayerOne[i].suit !== deckPlayerOne[j].suit) {
               if (positions.indexOf(j) === -1) {
                 positions.push(j);
               }
@@ -41,32 +38,11 @@ const DetGanadorProvider = ({children}) =>{
         while (positions.indexOf(x) !== -1) {
           x = Math.floor(Math.random() * (9 - 0 + 1) + 0);
         }
-        deckPlayerOne.splice(x, 1, data.cards[0]);
+        deckPlayerOne.splice(x, 1, data.cards[0]);  
 
-    }
-        else {
-            console.log("Soy el else")
-            deckPlayerOne.splice(2, 1, data.cards[0]);
-          /* let y = 0;
-          let banderaPrueba = false;
-          while (y < deckPlayerOne.length && banderaPrueba === false) {
-             const prueba = deckPlayerOne.filter(
-              (varPrueba) => varPrueba.value === deckPlayerOne[y].value
-            );
-            if(prueba.length<3){
-                deckPlayerOne.splice(y, 1, data.cards[0]);
-                banderaPrueba = true;
-                
-            }else{
-                y=y+1;
-            }
-          } */
+      }
 
-        }
-        
-
-      };
-
+      
 
       const getDeckPlayerOne = async () => {
         const url = `https://deckofcardsapi.com/api/deck/${IdPlayerOne}/draw/?count=10`;
